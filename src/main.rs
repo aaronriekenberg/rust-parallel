8995756bf180e5fea83ef46b0d63f34dd4d55174
dreampuf <soddyque@gmail.com>
2022-11-23 19:40:10 -0600

    Add struct CommandInfo.

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,15 @@ use tokio::{io::AsyncBufReadExt, process::Command, task::JoinSet};
 
 use tracing::debug;
 
-async fn run_command(command: String) -> String {
+#[derive(Debug)]
+struct CommandInfo {
+    _line_number: u64,
+    command: String,
+}
+
+async fn run_command(command_info: CommandInfo) -> CommandInfo {
     let command_output = Command::new("/bin/sh")
-        .args(["-c", &command])
+        .args(["-c", &command_info.command])
         .output()
         .await;
 
@@ -21,11 +27,11 @@ async fn run_command(command: String) -> String {
             }
         }
         Err(e) => {
-            tracing::warn!("got error running command '{}': {}", command, e);
+            tracing::warn!("got error running command '{:?}': {}", command_info, e);
         }
     };
 
-    command
+    command_info
 }
 
 #[tokio::main]
@@ -37,6 +43,7 @@ async fn main() -> anyhow::Result<()> {
     let mut reader = tokio::io::BufReader::new(tokio::io::stdin());
     let mut line = String::new();
     let mut join_set = JoinSet::new();
+    let mut line_number = 0u64;
 
     loop {
         line.clear();
@@ -49,6 +56,8 @@ async fn main() -> anyhow::Result<()> {
             break;
         }
 
+        line_number += 1;
+
         let trimmed_line = line.trim().to_owned();
 
         debug!("read line {}", trimmed_line);
@@ -57,7 +66,10 @@ async fn main() -> anyhow::Result<()> {
             continue;
         }
 
-        join_set.spawn(run_command(trimmed_line));
+        join_set.spawn(run_command(CommandInfo {
+            _line_number: line_number,
+            command: trimmed_line,
+        }));
     }
 
     debug!("after loop join_set.len() = {}", join_set.len());
