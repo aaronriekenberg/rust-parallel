62e6b1576ca4b63579fc229fff355c6a5a5f3f5e
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2023-04-22 10:08:20 -0500

    Add discard_output_mode option.

--- src/command_line_args.rs
+++ src/command_line_args.rs
@@ -1,6 +1,6 @@
 use anyhow::Context;
 
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 
 use tokio::sync::OnceCell;
 
@@ -15,6 +15,10 @@ use tracing::debug;
 #[derive(Parser, Debug, Default)]
 #[command(verbatim_doc_comment, version)]
 pub struct CommandLineArgs {
+    /// Discard output mode for commands
+    #[arg(short, long, value_enum, default_value_t = DiscardOutputMode::None)]
+    pub discard_output_mode: DiscardOutputMode,
+
     /// Input file or - for stdin.  Defaults to stdin if no inputs are specified.
     #[arg(short, long)]
     pub input: Vec<String>,
@@ -36,7 +40,7 @@ pub struct CommandLineArgs {
     pub shell: bool,
 
     /// Output channel capacity
-    #[arg(long, default_value_t = 1, value_parser = parse_semaphore_permits)]
+    #[arg(short, long, default_value_t = 1, value_parser = parse_semaphore_permits)]
     pub output_channel_capacity: usize,
 
     /// Optional command and initial arguments to run for each input line.
@@ -52,6 +56,24 @@ fn parse_semaphore_permits(s: &str) -> Result<usize, String> {
         Ok(value)
     } else {
         Err(format!("value not in range {:?}", range))
+    }
+}
+
+#[derive(Copy, Clone, Debug, ValueEnum)]
+pub enum DiscardOutputMode {
+    /// Capture stdout and stderr for child processes
+    None,
+    /// Redirect stdout for child processes to /dev/null
+    Stdout,
+    /// Redirect stderr for child processes to /dev/null
+    Stderr,
+    /// Redirect stdout and stderr for child processes to /dev/null
+    All,
+}
+
+impl Default for DiscardOutputMode {
+    fn default() -> Self {
+        Self::None
     }
 }
 
