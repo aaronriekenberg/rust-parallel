d4a143e426ada1ea9566af0a10bf3dd4f4f35f82
dreampuf <soddyque@gmail.com>
2023-06-23 15:54:50 -0500

    Rename variable to argument_groups.

--- src/parser/command_line.rs
+++ src/parser/command_line.rs
@@ -10,14 +10,14 @@ use crate::{
 };
 
 pub struct CommandLineArgsParser {
-    split_commands: VecDeque<Vec<String>>,
+    argument_groups: VecDeque<Vec<String>>,
     shell_enabled: bool,
     shell_command_and_args: Vec<String>,
 }
 
 impl CommandLineArgsParser {
     pub fn new(command_line_args: &CommandLineArgs) -> Self {
-        let split_commands = Self::build_split_commands(command_line_args);
+        let argument_groups = Self::build_argument_groups(command_line_args);
 
         let shell_command_and_args = if command_line_args.shell {
             vec![command_line_args.shell_path.clone(), "-c".to_owned()]
@@ -26,22 +26,22 @@ impl CommandLineArgsParser {
         };
 
         Self {
-            split_commands,
+            argument_groups,
             shell_enabled: command_line_args.shell,
             shell_command_and_args,
         }
     }
 
-    fn build_split_commands(command_line_args: &CommandLineArgs) -> VecDeque<Vec<String>> {
-        let mut split_commands =
+    fn build_argument_groups(command_line_args: &CommandLineArgs) -> VecDeque<Vec<String>> {
+        let mut argument_groups =
             VecDeque::with_capacity(command_line_args.command_and_initial_arguments.len());
 
-        let mut current_vec: Vec<String> = vec![];
+        let mut current_vec = vec![];
 
         for string in &command_line_args.command_and_initial_arguments {
             if string == COMMANDS_FROM_ARGS_SEPARATOR {
                 if !current_vec.is_empty() {
-                    split_commands.push_back(current_vec);
+                    argument_groups.push_back(current_vec);
                     current_vec = vec![];
                 }
             } else {
@@ -50,31 +50,31 @@ impl CommandLineArgsParser {
         }
 
         if !current_vec.is_empty() {
-            split_commands.push_back(current_vec);
+            argument_groups.push_back(current_vec);
         }
 
-        split_commands
+        argument_groups
     }
 
     pub fn parse_command_line_args(self) -> Vec<OwnedCommandAndArgs> {
-        let mut split_commands = self.split_commands;
+        let mut argument_groups = self.argument_groups;
 
-        let Some(first_command_and_args) = split_commands.pop_front() else {
+        let Some(first_command_and_args) = argument_groups.pop_front() else {
             return vec![];
         };
 
-        let split_args: Vec<Vec<String>> = split_commands
+        let arguments_list: Vec<Vec<String>> = argument_groups
             .into_iter()
             .multi_cartesian_product()
             .collect();
 
         trace!(
-            "first_command_and_args = {:?} split_args = {:?}",
+            "first_command_and_args = {:?} arguments_list = {:?}",
             first_command_and_args,
-            split_args,
+            arguments_list,
         );
 
-        let result = split_args
+        let result = arguments_list
             .into_iter()
             .map(|current_args| {
                 if self.shell_enabled {
