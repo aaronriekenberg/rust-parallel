bfb11c6a233b5e51ee64afb9587c1fa0f4f840af
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2023-07-04 08:19:05 -0500

    Update outline, use bash -e to fail on any command error.

--- scripts/generate_demos.sh
+++ scripts/generate_demos.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/bash -e
 
 RUST_PARALLEL="./target/debug/rust-parallel"
 VERSION=$($RUST_PARALLEL -V | cut -f2 -d' ')
 
-echo "# Demos (rust-parallel $VERSION)"
+echo "## Demos for rust-parallel $VERSION"
 
 echo 'There are 2 major ways to use rust-parallel:
 1. Run commands from arguments using `:::` syntax to separate argument groups similar to GNU parallel.
@@ -22,18 +22,18 @@ Demos of command line arguments mode are first as it is simpler to understand:
 1. [Calling a bash function commands from arguments](#calling-a-bash-function-commands-from-arguments)
 '
 
-echo '### Commands from arguments.
+echo '## Commands from arguments.
 
 The `:::` separator can be used to run the [Cartesian Product](https://en.wikipedia.org/wiki/Cartesian_product) of command line arguments.  This is similar to the `:::` behavior in GNU Parallel.
 '
 
 echo '```
 $ rust-parallel echo ::: A B ::: C D ::: E F G'
-$RUST_PARALLEL echo ::: A B ::: C D ::: E F G || exit 1
+$RUST_PARALLEL echo ::: A B ::: C D ::: E F G
 
 echo '
 $ rust-parallel echo hello ::: larry curly moe'
-$RUST_PARALLEL echo hello ::: larry curly moe || exit 1
+$RUST_PARALLEL echo hello ::: larry curly moe
 
 echo '
 # run gzip -k on all *.html files in current directory
@@ -46,11 +46,11 @@ If there is no leading command and args before the first `:::` then the cartesia
 
 echo '# runs ls and md5 on all combinations of argument groups:
 $ rust-parallel ::: ls md5 ::: /usr/share/dict/words /usr/share/dict/README ~/.profile'
-$RUST_PARALLEL ::: ls md5 ::: /usr/share/dict/words /usr/share/dict/README ~/.profile || exit 1
+$RUST_PARALLEL ::: ls md5 ::: /usr/share/dict/words /usr/share/dict/README ~/.profile
 echo '```'
 
 echo '
-### Small demo of echo commands.
+## Small demo of echo commands.
 
 Using command line arguments mode we can run 5 echo commands.
 
@@ -59,11 +59,11 @@ With `-j5` all commands run in parallel, with `-j1` commands run sequentially.
 
 echo '```
 $ rust-parallel -j5 echo ::: hi there how are you'
-$RUST_PARALLEL -j5 echo ::: hi there how are you || exit 1
+$RUST_PARALLEL -j5 echo ::: hi there how are you
 
 echo '
 $ rust-parallel -j1 echo ::: hi there how are you'
-$RUST_PARALLEL -j1 echo ::: hi there how are you || exit 1
+$RUST_PARALLEL -j1 echo ::: hi there how are you
 
 echo '```'
 
@@ -87,23 +87,20 @@ echo how
 echo are
 echo you
 EOL
-if [ $? -ne 0 ]; then
-  exit 1
-fi
 
 echo '
 $ cat test | rust-parallel -j5'
-cat test | $RUST_PARALLEL -j5 || exit 1
+cat test | $RUST_PARALLEL -j5
 
 echo '
 $ cat test | rust-parallel -j1'
-cat test | $RUST_PARALLEL -j1 || exit 1
+cat test | $RUST_PARALLEL -j1
 
 rm -f test
 
 echo '```'
 
-echo '### Debug logging.
+echo '## Debug logging.
 
 Set environment variable `RUST_LOG=debug` to see debug output.
 
@@ -114,44 +111,44 @@ Recommend enabling debug logging for all demos to understand what is happening i
 
 echo '```
 $ RUST_LOG=debug rust-parallel echo ::: hi there how are you | grep command_line_args | head -1'
-RUST_LOG=debug $RUST_PARALLEL echo ::: hi there how are you | grep command_line_args | head -1 | ansi-stripper || exit 1
+RUST_LOG=debug $RUST_PARALLEL echo ::: hi there how are you | grep command_line_args | head -1 | ansi-stripper
 
 echo '
 $ RUST_LOG=debug rust-parallel echo ::: hi there how are you | grep 'command_line_args:1''
-RUST_LOG=debug $RUST_PARALLEL echo ::: hi there how are you | grep 'command_line_args:1' | ansi-stripper || exit 1
+RUST_LOG=debug $RUST_PARALLEL echo ::: hi there how are you | grep 'command_line_args:1' | ansi-stripper
 echo '```'
 
-echo '### Specifying command and intial arguments on command line:
+echo '## Specifying command and intial arguments on command line:
 
 Here `md5 -s` will be prepended to each input line to form a command like `md5 -s aal`
 '
 
 echo '```
 $ head -100 /usr/share/dict/words | rust-parallel md5 -s | head -10'
-head -100 /usr/share/dict/words | $RUST_PARALLEL md5 -s | head -10 || exit 1
+head -100 /usr/share/dict/words | $RUST_PARALLEL md5 -s | head -10
 echo '```
 '
 
-echo '### Using `awk` to form complete commands:'
+echo '## Using `awk` to form complete commands:'
 
 echo '
 ```
 $ head -100 /usr/share/dict/words | awk '{printf "md5 -s %s\n", $1}' | rust-parallel | head -10'
-head -100 /usr/share/dict/words | awk '{printf "md5 -s %s\n", $1}' | $RUST_PARALLEL | head -10 || exit 1
+head -100 /usr/share/dict/words | awk '{printf "md5 -s %s\n", $1}' | $RUST_PARALLEL | head -10
 echo '```
 '
-echo '### Using as part of a shell pipeline.  
+echo '## Using as part of a shell pipeline.  
 
 stdout and stderr from each command run are copied to stdout/stderr of the rust-parallel process.
 '
 
 echo '```
 $ head -100 /usr/share/dict/words | rust-parallel md5 -s | grep -i abba'
-head -100 /usr/share/dict/words | $RUST_PARALLEL md5 -s | grep -i abba || exit 1
+head -100 /usr/share/dict/words | $RUST_PARALLEL md5 -s | grep -i abba
 echo '```
 '
 
-echo '### Working on a set of files from `find` command.  
+echo '## Working on a set of files from `find` command.  
 
 The `-0` option works nicely with `find -print0` to handle filenames with newline or whitespace characters:
 '
@@ -161,26 +158,26 @@ rm -fr testdir
 echo '```'
 echo '$ mkdir testdir
 '
-mkdir testdir || exit 1
+mkdir testdir
 
 echo "$ touch 'testdir/a b' 'testdir/b c' 'testdir/c d'
 "
-touch 'testdir/a b' 'testdir/b c' 'testdir/c d' || exit 1
+touch 'testdir/a b' 'testdir/b c' 'testdir/c d'
 
 echo '$ find testdir -type f -print0 | rust-parallel -0 gzip -f -k
 '
-find testdir -type f -print0 | $RUST_PARALLEL -0 gzip -f -k || exit 1
+find testdir -type f -print0 | $RUST_PARALLEL -0 gzip -f -k
 
 echo '$ ls -l testdir
 '
-ls -l testdir || exit 1
+ls -l testdir
 
 echo '```
 '
 
 rm -fr testdir
 
-echo '### Reading multiple inputs.
+echo '## Reading multiple inputs.
 
 By default `rust-parallel` reads input from stdin only.  The `-i` option can be used 1 or more times to override this behavior.  `-i -` means read from stdin, `-i ./test` means read from the file `./test`:
 '
@@ -196,19 +193,16 @@ foo
 bar
 baz
 EOL
-if [ $? -ne 0 ]; then
-  exit 1
-fi
 
 echo '
 $ head -5 /usr/share/dict/words | rust-parallel -i - -i ./test echo'
-head -5 /usr/share/dict/words | $RUST_PARALLEL -i - -i ./test echo || exit 1
+head -5 /usr/share/dict/words | $RUST_PARALLEL -i - -i ./test echo
 
 rm -f test
 
 echo '```'
 
-echo '### Calling a bash function.
+echo '## Calling a bash function.
 
 Use `-s` shell mode so that each input line is passed to `/bin/bash -c` as a single argument:
 '
@@ -241,20 +235,17 @@ doit 1
 doit 2
 doit 3
 EOL
-if [ $? -ne 0 ]; then
-  exit 1
-fi
 
 echo '
 $ cat test | rust-parallel -s'
 
-cat test | $RUST_PARALLEL -s || exit 1
+cat test | $RUST_PARALLEL -s
 rm -f test
 
 echo '```
 '
 
-echo '### Calling a bash function commands from arguments.
+echo '## Calling a bash function commands from arguments.
 
 Commands from arguments can also be used to invoke a bash function:
 '
@@ -274,6 +265,6 @@ export -f logargs
 
 echo '
 $ rust-parallel -s logargs ::: A B C ::: D E F'
-$RUST_PARALLEL -s logargs ::: A B C ::: D E F || exit 1
+$RUST_PARALLEL -s logargs ::: A B C ::: D E F
 
 echo '```'
