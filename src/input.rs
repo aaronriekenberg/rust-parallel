2cbd0f1e0bad2c05b13d1315a884ac4172350402
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2023-07-21 19:51:41 -0500

    Add input buffered_reader submodule.

--- src/input.rs
+++ src/input.rs
@@ -1,9 +1,9 @@
+mod buffered_reader;
 mod task;
 
 use anyhow::Context;
 
 use tokio::{
-    io::{AsyncBufRead, AsyncBufReadExt, BufReader, Split},
     sync::mpsc::{channel, Receiver},
     task::JoinHandle,
 };
@@ -83,73 +83,6 @@ fn build_input_list(command_line_args: &'static CommandLineArgs) -> InputList {
                 })
                 .collect(),
         )
-    }
-}
-
-type AsyncBufReadBox = Box<dyn AsyncBufRead + Unpin + Send>;
-
-struct BufferedInputReader {
-    buffered_input: BufferedInput,
-    split: Split<AsyncBufReadBox>,
-    next_line_number: usize,
-}
-
-impl BufferedInputReader {
-    async fn new(
-        buffered_input: BufferedInput,
-        command_line_args: &CommandLineArgs,
-    ) -> anyhow::Result<Self> {
-        let buf_reader = Self::create_buf_reader(buffered_input).await?;
-
-        let line_separator = if command_line_args.null_separator {
-            0u8
-        } else {
-            b'\n'
-        };
-
-        let split = buf_reader.split(line_separator);
-
-        Ok(Self {
-            buffered_input,
-            split,
-            next_line_number: 0,
-        })
-    }
-
-    async fn create_buf_reader(buffered_input: BufferedInput) -> anyhow::Result<AsyncBufReadBox> {
-        match buffered_input {
-            BufferedInput::Stdin => {
-                let buf_reader = BufReader::new(tokio::io::stdin());
-
-                Ok(Box::new(buf_reader))
-            }
-            BufferedInput::File { file_name } => {
-                let file = tokio::fs::File::open(file_name).await.with_context(|| {
-                    format!("error opening input file file_name = '{}'", file_name)
-                })?;
-                let buf_reader = BufReader::new(file);
-
-                Ok(Box::new(buf_reader))
-            }
-        }
-    }
-
-    async fn next_segment(&mut self) -> anyhow::Result<Option<(InputLineNumber, Vec<u8>)>> {
-        let segment = self.split.next_segment().await?;
-
-        match segment {
-            None => Ok(None),
-            Some(segment) => {
-                self.next_line_number += 1;
-
-                let input_line_number = InputLineNumber {
-                    input: Input::Buffered(self.buffered_input),
-                    line_number: self.next_line_number,
-                };
-
-                Ok(Some((input_line_number, segment)))
-            }
-        }
     }
 }
 
