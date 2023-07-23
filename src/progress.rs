d47c68cf818634b89c2ee62de17d0ca5a4dbbc35
dreampuf <soddyque@gmail.com>
2023-07-23 17:04:02 -0500

    Update progress bar style.

--- src/progress.rs
+++ src/progress.rs
@@ -4,6 +4,9 @@ use tokio::time::Duration;
 
 use crate::command_line_args::CommandLineArgs;
 
+const PROGRESS_STYLE: &str =
+    "{spinner} [{elapsed_precise}] Commands Done/Total: {pos:>2}/{len:2} {wide_bar} ETA {eta_precise}";
+
 pub struct Progress {
     progress_bar: Option<ProgressBar>,
 }
@@ -13,11 +16,9 @@ impl Progress {
         let progress_bar = if command_line_args.progress_bar {
             let progress_bar = ProgressBar::new(10);
 
-            progress_bar.enable_steady_tick(Duration::from_millis(200));
+            progress_bar.enable_steady_tick(Duration::from_millis(100));
 
-            let style = ProgressStyle::with_template(
-                "{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] Commands Done/Total: {pos:>7}/{len:7} ({eta})"
-            ).unwrap().progress_chars("#>-");
+            let style = ProgressStyle::with_template(PROGRESS_STYLE).unwrap();
 
             progress_bar.set_style(style);
 
