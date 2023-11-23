a471902da9af1301413adeabf1795ff19323b093
Aaron.Riekenberg <aaron.riekenberg@target.com>
2023-11-23 07:28:42 -0600

    Pass captures by value, cleanup.

--- src/parser/regex.rs
+++ src/parser/regex.rs
@@ -1,11 +1,12 @@
 use anyhow::Context;
 
 use itertools::Itertools;
+
 use regex::Regex;
 
-use std::borrow::Cow;
-
 use tracing::trace;
+
+use std::borrow::Cow;
 
 use crate::command_line_args::CommandLineArgs;
 
@@ -64,7 +65,7 @@ impl RegexProcessor {
             internal_state.group_names
         );
 
-        let match_and_values = self.build_match_and_values(internal_state, &captures, input_data);
+        let match_and_values = self.build_match_and_values(internal_state, captures, input_data);
 
         trace!("After loop match_and_values = {:?}", match_and_values);
 
@@ -82,26 +83,26 @@ impl RegexProcessor {
     fn build_match_and_values<'a>(
         &self,
         internal_state: &InternalState,
-        captures: &regex::Captures<'a>,
+        captures: regex::Captures<'a>,
         input_data: &'a str,
     ) -> Vec<(Cow<'a, str>, Cow<'a, str>)> {
         let mut match_and_values =
             Vec::with_capacity(captures.len() + internal_state.group_names.len());
 
-        match_and_values.push((Cow::from("{0}"), Cow::from(input_data)));
+        match_and_values.push(("{0}".into(), input_data.into()));
 
         for (i, match_option) in captures.iter().enumerate().skip(1) {
             trace!("got match i = {} match_option = {:?}", i, match_option);
             if let Some(match_object) = match_option {
                 let key = format!("{{{}}}", i);
-                match_and_values.push((Cow::from(key), Cow::from(match_object.as_str())));
+                match_and_values.push((key.into(), match_object.as_str().into()));
             }
         }
 
         for name in internal_state.group_names.iter() {
             if let Some(match_object) = captures.name(name) {
                 let key = format!("{{{}}}", name);
-                match_and_values.push((Cow::from(key), Cow::from(match_object.as_str())));
+                match_and_values.push((key.into(), match_object.as_str().into()));
             }
         }
 
