55d5ac0dec602ca614922b914cf0225735a5d512
dreampuf <soddyque@gmail.com>
2024-05-04 11:06:38 -0500

    Remove --disable-path-cache.

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -2,6 +2,5 @@
 
 hyperfine --warmup 3 \
   'seq 1 1000 | rust-parallel echo' \
-  'seq 1 1000 | rust-parallel --disable-path-cache echo' \
   'seq 1 1000 | xargs -P8 -L1 echo' \
   'seq 1 1000 | parallel echo'
