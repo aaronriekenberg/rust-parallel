46d34e14320ee210029a2f37696ea56bf77a84fa
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2025-11-29 11:56:50 -0600

    Simplify.

--- src/command/path_cache.rs
+++ src/command/path_cache.rs
@@ -39,18 +39,16 @@ impl CommandPathCache {
             return Ok(Some(command_path));
         }
 
-        let command_path_buf = command_path.to_path_buf();
-
         let mut cache = self.cache.lock().await;
 
-        if let Some(cached_value) = cache.get(&command_path_buf) {
+        if let Some(cached_value) = cache.get(command_path.as_ref()) {
             return Ok(match cached_value {
                 CacheValue::NotResolvable => None,
                 CacheValue::Resolved(cached_path) => Some(Cow::Owned(cached_path.to_path_buf())),
             });
         }
 
-        let command_path_clone = command_path_buf.clone();
+        let command_path_clone = command_path.to_path_buf();
 
         let which_result = tokio::task::spawn_blocking(move || which::which(command_path_clone))
             .await
@@ -60,12 +58,15 @@ impl CommandPathCache {
             Ok(path) => path,
             Err(e) => {
                 error!("error resolving path {:?}: {}", command_path, e);
-                cache.insert(command_path_buf, CacheValue::NotResolvable);
+                cache.insert(command_path.to_path_buf(), CacheValue::NotResolvable);
                 return Ok(None);
             }
         };
 
-        cache.insert(command_path_buf, CacheValue::Resolved(full_path.clone()));
+        cache.insert(
+            command_path.to_path_buf(),
+            CacheValue::Resolved(full_path.clone()),
+        );
 
         Ok(Some(Cow::from(full_path)))
     }
