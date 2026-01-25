00ab4f98e4472930993b3202af889a7127d71826
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2026-01-25 08:54:13 -0600

    wording

--- scripts/generate_manual.sh
+++ scripts/generate_manual.sh
@@ -242,7 +242,7 @@ $ RUST_LOG=debug rust-parallel echo ::: hi there how are you | grep 'command_lin
 RUST_LOG=debug $RUST_PARALLEL echo ::: hi there how are you | grep 'command_line_args:1'
 echo '```'
 
-echo 'If [pipe mode](#pipe-mode) is being used debug logs will show information line ranges and stdin block sizes:'
+echo 'If [pipe mode](#pipe-mode) is being used debug logs will show stdin line ranges and block sizes for each command:'
 echo '```'
 echo '$ cat /usr/share/dict/words| RUST_LOG=debug rust-parallel --pipe wc -l | grep spawned'
 cat /usr/share/dict/words | RUST_LOG=debug $RUST_PARALLEL --pipe wc -l | grep spawned
