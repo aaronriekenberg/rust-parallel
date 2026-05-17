9e3b1c28a415b3d1ccf4f2db729d954bee9854a1
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2026-05-17 11:19:18 -0500

    seq 1 100

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 hyperfine --warmup 3 \
-  'seq 1 1000 | rust-parallel echo' \
-  'seq 1 1000 | xargs -P8 -L1 echo' \
-  'seq 1 1000 | parallel echo'
+  'seq 1 100 | rust-parallel echo' \
+  'seq 1 100 | xargs -P8 -L1 echo' \
+  'seq 1 100 | parallel echo'
