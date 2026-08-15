f8abe36065da6337dc3dc4ae5b66d679b10b540c
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2026-08-15 10:31:02 -0500

    add shell quote to manual

--- scripts/generate_manual.sh
+++ scripts/generate_manual.sh
@@ -29,6 +29,7 @@ echo '
    1. [Numbered Capture Groups](#numbered-capture-groups)
    1. [Capture Group Special Characters](#capture-group-special-characters)
 1. [Shell Commands](#shell-commands)
+1. [Shell Quote](#shell-quote)
 1. [Bash Function](#bash-function)
    1. [Function Setup](#function-setup)
    1. [Demo of command line arguments](#demo-of-command-line-arguments)
@@ -423,6 +424,26 @@ Environment variables, `$` characters, nested commands and much more are possibl
 echo '```'
 echo -e '$ rust-parallel -s -r \x27(?P<arg1>.*) (?P<arg2>.*)\x27 \x27FOO={arg1}; BAR={arg2}; echo "FOO = ${FOO}, BAR = ${BAR}, shell pid = $$, date = $(date)"\x27 ::: A B ::: CAT DOG'
 $RUST_PARALLEL -s -r '(?P<arg1>.*) (?P<arg2>.*)' 'FOO={arg1}; BAR={arg2}; echo "FOO = ${FOO}, BAR = ${BAR}, shell pid = $$, date = $(date)"' ::: A B ::: CAT DOG
+echo '```'
+
+echo '## Shell Quote
+
+When using shell mode (`-s`), the `--shell-quote` option applies shell escaping to each argument before passing it to the shell.
+
+This is useful when arguments contain special shell characters like `$`, backticks, single quotes, or double quotes that should be treated as literal strings and not interpreted by the shell.
+
+Without `--shell-quote`, special characters can cause command injection or unexpected shell behavior. With `--shell-quote` enabled, arguments are properly escaped.
+'
+
+echo '```'
+echo "$ echo -e \"hello\$world\nfoo\`cmd\`\" | rust-parallel -s -0 --shell-quote echo"
+echo -e "hello\$world\nfoo\`cmd\`" | $RUST_PARALLEL -s -0 --shell-quote echo
+echo '```'
+
+echo 'Without `--shell-quote`, the special characters would be interpreted by the shell:'
+echo '```'
+echo "$ echo -e \"hello\$world\nfoo\`cmd\`\" | rust-parallel -s -0 echo"
+echo -e "hello\$world\nfoo\`cmd\`" | $RUST_PARALLEL -s -0 echo
 echo '```'
 
 echo '## Bash Function
